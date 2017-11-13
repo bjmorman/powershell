638adc1ad897c4fe099977572d604f0a9eeb1657
DeVore Tech <dTech@devore.com>
2017-11-12 22:42:14 -0500

    adjustments for older powershell versions and inconsistent results

--- Shares/remap-drives.ps1
+++ Shares/remap-drives.ps1
@@ -51,17 +51,17 @@ poweshell.exe remap-drives.ps1 -domain 'example.net' -oldHost 'old-fs' -newHost 
 [CmdletBinding()]
 Param(
   [Parameter(Mandatory=$True)]
-   [string]domain,
+   [string]$domain,
   [Parameter(Mandatory=$True)]
-   [string]oldHost,
+   [string]$oldHost,
   [Parameter(Mandatory=$True)]
-   [string]newHost,
+   [string]$newHost,
   [Parameter(Mandatory=$False)]
-   [boolean]reMap=$False,
+   [boolean]$reMap=$False,
   [Parameter(Mandatory=$False)]
-   [boolean]logOutput=$False,
+   [boolean]$logOutput=$False,
   [Parameter(Mandatory=$False)]
-   [string]logOutputPath
+   [string]$logOutputPath
 )
 
 # TODO: check for open files
@@ -71,9 +71,8 @@ Function Get-Mappings {
     param ( $oldHostShort,
             $oldHostLong )
 
-    $mappings = @()
-    $mappings += Get-PSDrive -PSProvider FileSystem | ?{$_.DisplayRoot -like $oldHostShort -or $_.DisplayRoot -like $oldHostLong} | Select Name, Root, DisplayRoot
-    $mappings += Get-PSDrive -PSProvider FileSystem | ?{$_.Root -like $oldHostShort -or $_.Root -like $oldHostLong} | Select Name, Root, DisplayRoot
+    # using Get-WmiObject instead of Get-PSDrive due to powershell version differences and inconsistent results
+    $mappings += Get-WmiObject Win32_MappedLogicalDisk | ?{$_.ProviderName -like $oldHostShort -or $_.ProviderName -like $oldHostLong} |Select Name, ProviderName
 
     Return $mappings
 }
@@ -90,69 +89,90 @@ Function Parse-Mappings {
         $path = $null
         $newPath = $null
 
-        If ($_.Root -like $oldHostShort -or $_.Root -like $oldHostLong) {
-            $path = $_.Root
-            $newPath = ($_.Root).replace($oldHost, $newHost)
-        } Else {
-            $path = $_.DisplayRoot
-            $newPath = ($_.DisplayRoot).replace($oldHost, $newHost)
+        If ($_.ProviderName -like $oldHostShort -or $_.ProviderName -like $oldHostLong) {
+            $path = $_.ProviderName
+            $newPath = ($_.ProviderName.ToLower()).replace($oldHost.ToLower(), $newHost.ToLower())
+
+            $props = @{
+                drive = $_.Name
+                oldPath = $path
+                newPath = $newPath
+            }
+            $coMapping = New-Object PSObject -Property $props
         }
 
-        $pscoMapping = [PSCustomObject]@{
-            drive    = $_.Name
-            oldPath  = $path
-            newPath  = $newPath
-        }
+        # does not work in older powershell versions
+        #$coMapping = [PSCustomObject]@{
+        #    drive    = $_.Name
+        #    oldPath  = $path
+        #    newPath  = $newPath
+        #}
     
-        $parsedMappings += $pscoMapping
+        $parsedMappings += $coMapping
     }
     return $parsedMappings
 }
 
+Function Validate-Mappings {
+    param ( $parsedMappings )
+
+    # some mappings can falsely appear in Get-WmiObject Win32_MappedLogicalDisk,
+    # this removes them
+    $validatedMappings = @()
+    $netUseOutput = Invoke-Expression "net use"
+    $parsedMappings | % {
+        if ($netuseOutput | Select-String $_.drive) {
+            $validatedMappings += $_
+        }
+    }
+    Return $validatedMappings
+}
+
 Function Log-Output {
-    param ( $drives,
+    param ( $parsedMappings,
             $logOutputPath )
 
-    $fileName = (Invoke-Expression whoami).split("\")[1] + ".csv"
+    $fileName = (Invoke-Expression whoami).split("\")[1] + ".txt"
     $filePath = $logOutputPath + $fileName
-    $drives | Export-Csv -Path $filePath -Force
+    $parsedMappings | Out-File -FilePath $filePath -Force
 }
 
 Function Re-Map {
-    param ( $drives )
+    param ( $parsedMappings )
 
-    ForEach ($drive in $drives) {
+    ForEach ($drive in $parsedMappings) {
         #out with the old
         $delCmd = "net use " + $drive.drive + ": /DELETE /YES"
-        Invoke-Expression $delCmd | Out-File c:\1.txt
+        Invoke-Expression $delCmd
 
         #in with the new
         $newCmd = "net use " + $drive.drive + ": " + $drive.newPath
-        Invoke-Expression $newCmd | Out-File c:\2.txt
+        Invoke-Expression $newCmd
     }
 }
 
 # main
 # retrieve current mappings
-$oldHostShort = "\\" + $oldHost + "\*"
-$oldHostLong = "\\" + $oldHost + "." + $domain + "\*"
-$newHostShort = "\\" + $newHost + "\*"
-$newHostLong = "\\" + $newHost + "." + $domain + "\*"
+$oldHostShort = "\\" + $oldHost.ToLower() + "\*"
+$oldHostLong = "\\" + $oldHost.ToLower() + "." + $domain + "\*"
+$newHostShort = "\\" + $newHost.ToLower() + "\*"
+$newHostLong = "\\" + $newHost.ToLower() + "." + $domain + "\*"
 
 $currentMappings = $Null
-$drives = $Null
+$parsedMappings = $Null
 $currentMappings = Get-Mappings $oldHostShort $oldHostLong
 
 if ($currentMappings) {
     # do the deed
-    $drives = Parse-Mappings $currentMappings $oldHostShort $oldHostLong $newHostShort $newHostLong
+    $parsedMappings = Parse-Mappings $currentMappings $oldHostShort $oldHostLong $newHostShort $newHostLong
+    $validatedMappings = Validate-Mappings $parsedMappings
 
     if ($logOutput) {
-        Log-Output $drives $logOutputPath
+        Log-Output $validatedMappings $logOutputPath
     }
 
 #    If ($reMap) {
-#        Re-Map $drives
+#        Re-Map $validatedMappings
 #    }
 } Else {
     # do nothing
